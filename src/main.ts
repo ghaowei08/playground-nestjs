7eb1bfb8a0dc453c703844ef997be74dc5467898
Haowei Goh <43960083+ghaowei08@users.noreply.github.com>
2023-04-15 18:16:04 +0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const port = process.env.APP_PORT ?? 3000;
+  await app.listen(port);
+  Logger.log(`APP START AT PORT ${port}`);
 }
 bootstrap();
